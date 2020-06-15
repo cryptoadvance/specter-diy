44112035bdd67dd1965ec71650a6d43deeac03c5
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-06-15 18:10:10 +0200

    wallet screen

--- src/gui/screens/__init__.py
+++ src/gui/screens/__init__.py
@@ -1,4 +1,4 @@
 from .base import Screen, MenuScreen, Alert, Prompt, QRAlert
 from .input import PinScreen, InputScreen, DerivationScreen
 from .mnemonic import MnemonicScreen, NewMnemonicScreen, RecoverMnemonicScreen
-from .bitcoin import XPubScreen
+from .bitcoin import XPubScreen, WalletScreen
