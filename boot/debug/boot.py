26d5702ccaeb652e3652279f0a7ef165b6cdb1b5
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-04-08 10:32:22 +0200

    bump version

--- boot/debug/boot.py
+++ boot/debug/boot.py
@@ -6,10 +6,10 @@ import pyb, os, micropython, time
 pwr = pyb.Pin("B15", pyb.Pin.OUT)
 pwr.on()
 
-# v1.5.1-rc1 - use odd rc for main firmware and even for debug
+# v1.5.2-rc1 - use odd rc for main firmware and even for debug
 # so it's possible to upgrade from debug to main firmware.
 # (rc99 is final version for production)
-version = "<version:tag10>0100500101</version:tag10>"
+version = "<version:tag10>0100500201</version:tag10>"
 
 leds = [pyb.LED(i) for i in range(1,5)]
 # poweroff on button press
