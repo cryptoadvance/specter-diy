3453b86c923b7dd1fec031953d090667b74e31b6
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-10-27 13:05:57 +0200

    bump version

--- boot/main/boot.py
+++ boot/main/boot.py
@@ -13,10 +13,11 @@ for p in sys.path:
 pwr = pyb.Pin("B15", pyb.Pin.OUT)
 pwr.on()
 
-# v1.6.0-rc99 - use odd rc for main firmware and even for debug
+# v1.6.1-rc99 - rc99 is the production release.
+# Use odd rc for main firmware and even for debug
 # so it's possible to upgrade from debug to main firmware.
 # (rc99 is final version for production)
-version = "<version:tag10>0100600099</version:tag10>"
+version = "<version:tag10>0100600199</version:tag10>"
 
 # get i2c
 i2c = pyb.I2C(1)
