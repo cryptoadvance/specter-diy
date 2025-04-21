a842a0fbeb0a2a8249455e45eaab4dda906752b5
3rd Iteration <steve@cryptoguide.tips>
2025-04-21 17:34:29 -0400

    Initial Scanner Update
    *Add version check and set RAW mode for readers that need this
    *Report detected scanner SW version on the QR scanner screen

--- src/hosts/qr.py
+++ src/hosts/qr.py
@@ -41,6 +41,16 @@ which happens if we scan a different qr code. """
 DELAY_OF_SAME_BARCODES_ADDR = b"\x00\x13"
 DELAY_OF_SAME_BARCODES = 0x85  # 5 seconds
 
+""" Address for software version check.
+(The ability to check firmware version via this memory was removed from newer GM65 type devices, but still lets us identify versions that need a fix)"""
+VERSION_ADDR = b"\x00\xE2"
+VERSION_NEEDS_RAW = 0x69  # A version of GM65 that needs RAW mode to be turned on...
+
+""" Some newer GM65 Scanners need to be set to "RAW" mode to correctly scan binary QR codes.
+(The address for this is actually undocumented and doesn't seem to work the same on newer varients like the GM805)"""
+RAW_MODE_ADDR = b"\x00\xBC"
+RAW_MODE_VALUE = 0x08
+
 
 class QRHost(Host):
     """
@@ -55,6 +65,9 @@ class QRHost(Host):
 
     button = "Scan QR code"
     settings_button = "QR scanner"
+
+    # Some Information to report device to user
+    version_str = "No Scanner Detected"
 
     def __init__(self, path, trigger=None, uart="YA", baudrate=9600):
         super().__init__(path)
@@ -90,13 +103,13 @@ class QRHost(Host):
 
     @property
     def MASK(self):
-        b = (1<<7)
+        b = (1 << 7)
         if self.settings.get("sound", True):
-            b |= (1<<6)
+            b |= (1 << 6)
         if self.settings.get("aim", True):
-            b |= (1<<4)
+            b |= (1 << 4)
         if self.settings.get("light", False):
-            b |= (1<<2)
+            b |= (1 << 2)
         return b
 
     @property
@@ -142,6 +155,8 @@ class QRHost(Host):
     def configure(self):
         """Tries to configure scanner, returns True on success"""
         save_required = False
+
+        # Set Serial Output Mode
         val = self.get_setting(SERIAL_ADDR)
         if val is None:
             return False
@@ -149,6 +164,7 @@ class QRHost(Host):
             self.set_setting(SERIAL_ADDR, val & 0xFC)
             save_required = True
 
+        # Set Command Mode
         val = self.get_setting(SETTINGS_ADDR)
         if val is None:
             return False
@@ -156,6 +172,7 @@ class QRHost(Host):
             self.set_setting(SETTINGS_ADDR, self.CMD_MODE)
             save_required = True
 
+        # Set scanning timeout
         val = self.get_setting(TIMOUT_ADDR)
         if val is None:
             return False
@@ -163,6 +180,7 @@ class QRHost(Host):
             self.set_setting(TIMOUT_ADDR, 0)
             save_required = True
 
+        # Set interval between scans
         val = self.get_setting(INTERVAL_OF_SCANNING_ADDR)
         if val is None:
             return False
@@ -170,6 +188,7 @@ class QRHost(Host):
             self.set_setting(INTERVAL_OF_SCANNING_ADDR, INTERVAL_OF_SCANNING)
             save_required = True
 
+        # Set delay beteen re-reading the same barcode
         val = self.get_setting(DELAY_OF_SAME_BARCODES_ADDR)
         if val is None:
             return False
@@ -177,6 +196,20 @@ class QRHost(Host):
             self.set_setting(DELAY_OF_SAME_BARCODES_ADDR, DELAY_OF_SAME_BARCODES)
             save_required = True
 
+        # Check the module software and enable "RAW" mode if required
+        val = self.get_setting(VERSION_ADDR)
+        if val is None:
+            return False
+        self.version_str = "Detected GM65 Scanner, SW:" + str(val)
+        if val == VERSION_NEEDS_RAW:
+            val = self.get_setting(RAW_MODE_ADDR)
+            if val is None:
+                return False
+            if val != RAW_MODE_VALUE:
+                self.set_setting(RAW_MODE_ADDR, RAW_MODE_VALUE)
+                save_required = True
+
+        # Save settings to EEPROM if anything has changed.
         if save_required:
             val = self.save_settings_on_scanner()
             if not val:
@@ -218,6 +251,7 @@ class QRHost(Host):
 
     async def settings_menu(self, show_screen, keystore):
         title = "QR scanner"
+        note = self.version_str
         controls = [{
             "label": "Enable QR scanner",
             "hint": "Enable or disable QR scanner and remove corresponding button from the main menu",
@@ -235,7 +269,7 @@ class QRHost(Host):
             "hint": "Can create blicks on the screen",
             "value": self.settings.get("light", False)
         }]
-        scr = HostSettings(controls, title=title)
+        scr = HostSettings(controls, title=title, note=note)
         res = await show_screen(scr)
         if res:
             enabled, sound, aim, light = res
@@ -254,7 +288,7 @@ class QRHost(Host):
 
     def _stop_scanner(self):
         if self.trigger is not None:
-            self.trigger.on() # trigger is reversed, so on means disable
+            self.trigger.on()  # trigger is reversed, so on means disable
         else:
             self.set_setting(SCAN_ADDR, 0)
 
@@ -278,21 +312,21 @@ class QRHost(Host):
         self._stop_scanner()
 
     def abort(self):
-        with open(self.tmpfile,"wb"):
+        with open(self.tmpfile, "wb"):
             pass
         self.cancelled = True
         self.stop_scanning()
 
     @property
     def tmpfile(self):
-        return self.path+"/tmp"
+        return self.path + "/tmp"
 
     async def scan(self, raw=True, chunk_timeout=0.5):
         self.raw = raw
         self.chunk_timeout = chunk_timeout
         self._start_scanner()
         # clear the data
-        with open(self.tmpfile,"wb") as f:
+        with open(self.tmpfile, "wb") as f:
             pass
         if self.f is not None:
             self.f.close()
@@ -319,14 +353,14 @@ class QRHost(Host):
         gc.collect()
         if self.cancelled:
             return None
-        self.f = open(self.path+"/data.txt", "rb")
+        self.f = open(self.path + "/data.txt", "rb")
         return self.f
 
     def check_animated(self, data: bytes):
         try:
             # should be only ascii characters
             d = data.decode().strip().lower()
-            if d.startswith("ur:"): # ur:bytes or ur:crypto-psbt
+            if d.startswith("ur:"):  # ur:bytes or ur:crypto-psbt
                 return True
             # this will raise if it's not a valid prefix
             self.parse_prefix(data.split(b" ")[0])
@@ -342,7 +376,7 @@ class QRHost(Host):
             return
         # read all available data
         if self.uart.any() > 0:
-            if not self.animated: # read only one QR code
+            if not self.animated:  # read only one QR code
                 # let all data to come on the first QR code
                 await asyncio.sleep(self.chunk_timeout)
                 d = self.uart.read()
@@ -361,14 +395,14 @@ class QRHost(Host):
                 d = self.uart.read()
             # no new lines - just write and continue
             if d[-len(self.EOL):] != self.EOL:
-                with open(self.tmpfile,"ab") as f:
+                with open(self.tmpfile, "ab") as f:
                     f.write(d)
                 return
             # restart scan while processing data
             await self._restart_scanner()
             # slice to write
             d = d[:-len(self.EOL)]
-            with open(self.tmpfile,"ab") as f:
+            with open(self.tmpfile, "ab") as f:
                 f.write(d)
             try:
                 if self.process_chunk():
@@ -451,7 +485,7 @@ class QRHost(Host):
                 # allocate stuff
                 self.animated = True
                 self.parts = [None] * n
-                fname = "%s/p%d.txt" % (self.path, m-1)
+                fname = "%s/p%d.txt" % (self.path, m - 1)
                 with open(fname, "wb") as fout:
                     read_write(f, fout)
                 self.parts[m - 1] = fname
@@ -467,7 +501,7 @@ class QRHost(Host):
         if hsh != self.bcur_hash:
             print(hsh, self.bcur_hash)
             raise HostError("Checksum mismatch")
-        fname = "%s/p%d.txt" % (self.path, m-1)
+        fname = "%s/p%d.txt" % (self.path, m - 1)
         with open(fname, "wb") as fout:
             fout.write(f.read())
         self.parts[m - 1] = fname
@@ -509,7 +543,7 @@ class QRHost(Host):
                     # allocate stuff
                     self.animated = True
                     self.parts = [None] * n
-                    fname = "%s/p%d.txt" % (self.path, m-1)
+                    fname = "%s/p%d.txt" % (self.path, m - 1)
                     with open(fname, "wb") as fout:
                         read_write(f, fout)
                     self.parts[m - 1] = fname
@@ -533,7 +567,7 @@ class QRHost(Host):
         m, n = self.parse_prefix(chunk)
         if n != len(self.parts):
             raise HostError("Invalid prefix")
-        fname = "%s/p%d.txt" % (self.path, m-1)
+        fname = "%s/p%d.txt" % (self.path, m - 1)
         with open(fname, "wb") as fout:
             read_write(f, fout)
         self.parts[m - 1] = fname
@@ -580,7 +614,7 @@ class QRHost(Host):
         note = meta.get("note")
         start = stream.read(4)
         stream.seek(-len(start), 1)
-        if start in [b"cHNi", b"cHNl"]: # convert from base64 for QR encoder
+        if start in [b"cHNi", b"cHNl"]:  # convert from base64 for QR encoder
             with open(self.tmpfile, "wb") as f:
                 a2b_base64_stream(stream, f)
             with open(self.tmpfile, "rb") as f:
@@ -592,13 +626,13 @@ class QRHost(Host):
             return await self.manager.gui.qr_alert(title, msg, response, note=note, qr_width=480)
 
         EncoderCls = None
-        if self.bcur2: # we need binary
+        if self.bcur2:  # we need binary
             from qrencoder import CryptoPSBTEncoder as EncoderCls
         elif self.bcur:
             from qrencoder import LegacyBCUREncoder as EncoderCls
         else:
             from qrencoder import Base64QREncoder as EncoderCls
-        with EncoderCls(stream, tempfile=self.path+"/qrtmp") as enc:
+        with EncoderCls(stream, tempfile=self.path + "/qrtmp") as enc:
             await self.manager.gui.qr_alert(title, "", enc, note=note, qr_width=480)
 
     @property
